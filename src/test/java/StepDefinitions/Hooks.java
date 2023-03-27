e0c50a556f3867532b51bbffaf91d5c616a381be
Naime Kaymaz <119721390+Naime922@users.noreply.github.com>
2023-03-27 13:34:05 -0700

    update

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -3,6 +3,15 @@ package StepDefinitions;
 import Utilities.BasicDriver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Hooks {
     @Before
@@ -11,7 +20,22 @@ public class Hooks {
     }
 
     @After
-    public void afterScenario(){
+    public void afterScenario(Scenario scenario){
+        if (scenario.isFailed()){
+
+           final byte[] byteImage=((TakesScreenshot) BasicDriver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(byteImage,"image/png","screenShot name");
+
+//            TakesScreenshot takesScreenshot= (TakesScreenshot) BasicDriver.getDriver();
+//            File file=takesScreenshot.getScreenshotAs(OutputType.FILE);
+//            LocalDateTime timeOfError=LocalDateTime.now();
+//            DateTimeFormatter formatter=DateTimeFormatter.ofPattern("MMddyyyyHHmmss");
+//           try {
+//                FileUtils.copyFile(file,new File("src/test/java/ScreenShots/screenshot"+timeOfError.format(formatter)+".png"));
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+        }
         BasicDriver.quitDriver();
         System.out.println("Scenario has ended");
     }
